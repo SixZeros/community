ea5bd4327a177e7feb1d02778fd5c1fbfbea5b4f
SixZeros <526492409@qq.com>
2022-01-03 21:29:38 +0800

    add AuthorizeController, AccessTokenDTO,GithubUser,GithubProvider | updata pom

---
+++ src/main/java/tian/life/community/controller/AuthorizeController.java
@@ -0,0 +1,34 @@
+package tian.life.community.controller;
+
+
+import com.sun.org.apache.bcel.internal.classfile.Code;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import tian.life.community.dto.AccessTokenDTO;
+import tian.life.community.dto.GithubUser;
+import tian.life.community.provider.GithubProvider;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("c5fa6e4a8d2a84df1460");
+        accessTokenDTO.setClient_secret("ecf5abf7a3c6e85bc1b005915fa0f6fa69c462d6");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}

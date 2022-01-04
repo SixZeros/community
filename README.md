93b20540431228092a7101f278246e90e80df1c9
SixZeros <526492409@qq.com>
2022-01-04 22:11:31 +0800

    连接数据库，并将用户的登录信息记录到数据库

--- README.md
+++ README.md
@@ -4,7 +4,12 @@
 [Spring web 文档](https://spring.io/guides/gs/serving-web-content/)
 [bootstrap文档](https://v3.bootcss.com/)
 [Github OAuth](https://docs.github.com/cn/developers/apps/building-oauth-apps/creating-an-oauth-app)
+[菜鸟教程](https://www.runoob.com/)
+[maven仓库](https://mvnrepository.com/)
+[mybatis](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-embedded-database-support)
+[Spring Dev Tool](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-embedded-database-support)
 
 
 ## 工具
 [Git](https://git-scm.com/download)
+[h2数据库](http://www.h2database.com/html/quickstart.html)
--- src/main/java/tian/life/community/controller/AuthorizeController.java
+++ src/main/java/tian/life/community/controller/AuthorizeController.java
@@ -11,9 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import tian.life.community.dto.AccessTokenDTO;
 import tian.life.community.dto.GithubUser;
+import tian.life.community.mapper.UserMapper;
+import tian.life.community.model.User;
 import tian.life.community.provider.GithubProvider;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -23,10 +26,15 @@ public class AuthorizeController {
 
     @Value("${github.client.id}")
     private String clientId;
+
     @Value("${github.client.secret}")
     private String clientSecret;
+
     @Value("${github.redirect.uri}")
     private String redirectUri;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
@@ -39,9 +47,16 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if (user != null) {
-            request.getSession().setAttribute("user", user);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("githubUser", githubUser);
             return "redirect:/";
             //登录成功，写cookie 和 session
         } else {
